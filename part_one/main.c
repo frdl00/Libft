054c445d65485b996cc2bfa013a5a545af228d8c
Fatma Nur Ardal <fardal@k1m05s01.42istanbul.com.tr>
2026-01-27 20:05:03 +0300

    aptal

---
+++ part_one/main.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char i[] = "bu bilgisayarı beyinsizin teki kullanıyor.";
+    char m[] = "kopyalanacakstring";
+
+    // ft_memcpy(i, m, 5);
+    // printf("%s", i);
+
+    // ft_memmove(m + 6, m, 8);
+    // printf("%s" , m);
+
+    // ft_memset(i, 't', 10);
+    // printf("%s", i);
+
+    ft_strchr(i, 't');
+    
+}
